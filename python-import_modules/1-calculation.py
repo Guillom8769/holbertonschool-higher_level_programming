1a467b7af20a2f1b719126976a0d50afc9913eb8
Guillom8769 <8769@holbertonstudents.com>
2024-05-07 22:17:06 +0200

    modification ecriture en formatage generique et type explicitement spécifié :d

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    # Importer les fonctions depuis calculator_1
     from calculator_1 import add, sub, mul, div
-
-    # Définir les variables a et b
     a = 10
     b = 5
-
-    # Effectuer et afficher les opérations
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
