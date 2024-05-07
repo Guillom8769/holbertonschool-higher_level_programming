7c60d28ec1ab54257c4530cd812c38c5e19f4c1d
Guillom8769 <8769@holbertonstudents.com>
2024-05-07 20:41:32 +0200

    fix indentation

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+if __name__ == "__main__":  # if the script is executed, not imported
 
-# Importer uniquement la fonction add depuis add_0
-from add_0 import add
+    # Importer uniquement la fonction add depuis add_0
+    from add_0 import add
 
-# Définir les variables
-a = 1
-b = 2
+    # Définir les variables
+    a = 1
+    b = 2
 
-# Appeler la fonction `add` et récupérer le résultat
-result = add(a, b)
+    # Appeler la fonction `add` et récupérer le résultat
+    result = add(a, b)
 
-# Imprimer le résultat en utilisant la méthode `format`
-print("{} + {} = {}".format(a, b, result))
+    # Imprimer le résultat en utilisant la méthode `format`
+    print("{} + {} = {}".format(a, b, result))
