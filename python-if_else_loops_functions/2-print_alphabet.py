2dc86f217467a617eea9619123df3c26541a92dc
Guillom8769 <8769@holbertonstudents.com>
2024-05-07 10:14:48 +0200

    fix code

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-ascii_code_of_a = 97
-ascii_code_of_z = 123
-for ascii_code in range(ascii_code_of_a, ascii_code_of_z):
-    print(chr(ascii_code), end='')
+code_of_a = 97
+code_of_z = 123
+for code in range(code_of_a, code_of_z):
+    print(chr(code), end='')
