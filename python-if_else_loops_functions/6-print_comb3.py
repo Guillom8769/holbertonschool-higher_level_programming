950204c521481c2e487feb292b022a5d64d563da
Guillom8769 <8769@holbertonstudents.com>
2024-05-07 09:36:15 +0200

    fix pycode

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        if i < j:
-            print(f"{i}{j}", end=", " if j < 9 or i < 8 else "\n")
+        print(f"{i}{j}", end=", " if j < 9 or i < 8 else "\n")
