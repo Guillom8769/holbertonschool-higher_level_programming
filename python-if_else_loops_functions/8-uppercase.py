61069a034fa7b0e6edc3626ac7a91ede3c70534d
Guillom8769 <8769@holbertonstudents.com>
2024-05-07 11:59:33 +0200

    function that prints a string in uppercase followed by a new line.

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uppercase(str):
+    """
+    Convertit une chaîne de caractères en majuscules et l'affiche.
+    """
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            # Convertir le caractère minuscule en majuscule
+            upper_c = chr(ord(c) - 32)
+        else:
+            upper_c = c
+        print(f"{upper_c}", end="")
+    print()
