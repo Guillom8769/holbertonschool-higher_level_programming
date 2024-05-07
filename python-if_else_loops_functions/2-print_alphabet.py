4443a202df863b567a40e3216038f36e6c651b2f
Guillom8769 <8769@holbertonstudents.com>
2024-05-07 10:16:16 +0200

    change code

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-code_of_a = 97
-code_of_z = 123
-for code in range(code_of_a, code_of_z):
-    print(chr(code), end='')
+for i in range(97, 123):
+    print("{:s}".format(chr(i)), end="")
