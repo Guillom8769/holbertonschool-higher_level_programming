bbf1dc42626a138dfc3792cce5cd9fdd05455d0d
Guillom8769 <8769@holbertonstudents.com>
2024-05-07 21:01:26 +0200

    fix if name

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-
-# Importer la fonction add depuis le fichier add_0
-from add_0 import add
-
-# Définir les variables a et b
-a = 1
-b = 2
-
-# Calculer le résultat en appelant la fonction add
-result = add(a, b)
-
-# Afficher le résultat avec `format`
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    
