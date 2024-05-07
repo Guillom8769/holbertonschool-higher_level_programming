f960519d07f61268797e97596187c5c2ea9cf301
Guillom8769 <8769@holbertonstudents.com>
2024-05-07 11:10:43 +0200

    change code

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for current_number in range(100):
-    # Applique un formatage à deux chiffres avec un remplissage de zéro pour les nombres inférieurs à 10
-    if current_number < 99:
-        print(f"{current_number:02}, ", end="")
+
+for number in range(0, 100):
+    if number != 99:
+        print("{:02d}, ".format(number), end="")
     else:
-        # Le dernier nombre est suivi par un saut de ligne
-        print(f"{current_number:02}")
+        print("{:02d}".format(number))
