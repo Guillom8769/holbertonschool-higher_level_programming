f7187028714618ee0b83e39ff6cde06c54cc7c54
Guillom8769 <8769@holbertonstudents.com>
2024-05-07 10:04:03 +0200

    fix pycode

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for ascii_code in range(97, 123):  # ASCII codes for 'a' to 'z'
+for ascii_code in range(97, 123):
     letter = chr(ascii_code)
     if letter != 'q' and letter != 'e':
         print(f"{letter}", end="")
