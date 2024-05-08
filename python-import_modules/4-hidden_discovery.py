f4f0be741cd11eb2a54cd7d60151babd8caddee9
Guillom8769 <8769@holbertonstudents.com>
2024-05-08 12:58:40 +0200

    change code

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    # Importer le module compilé hidden_4
     import hidden_4
 
-    # Obtenir la liste de tous les noms dans hidden_4
-    names = dir(hidden_4)
-
-    # Trier et filtrer les noms qui ne commencent pas par '__'
-    for name in sorted(names):
+    # Trier les noms et filtrer ceux qui ne commencent pas par '__'
+    for name in sorted(dir(hidden_4)):
         if not name.startswith("__"):
             print(name)
