2f058dcbd2919616d46a900e6139e95c6e3a27e4
Guillom8769 <8769@holbertonstudents.com>
2024-05-08 13:02:40 +0200

    use code origin

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    # Importer le module compilé hidden_4
     import hidden_4
 
-    for i in sorted(dir(hidden_4)):
-        if i[:2] != "__":
-            print(i)
+    # Obtenir la liste de tous les noms dans hidden_4
+    names = dir(hidden_4)
+
+    # Trier et filtrer les noms qui ne commencent pas par '__'
+    for name in sorted(names):
+        if not name.startswith("__"):
+            print(name)
