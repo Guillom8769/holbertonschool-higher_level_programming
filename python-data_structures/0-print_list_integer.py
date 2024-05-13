482854121eb1e54b52d3f0e1f16e91b55cca3ac6
Guillom8769 <8769@holbertonstudents.com>
2024-05-13 11:40:45 +0200

    Firts commit for folder and readme and task 0: unction that prints all integers of a list.

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """
+    Prints all integers from a list, one per line.
+    
+    Args:
+    my_list (list): A list of integers.
+    """
+    for number in my_list:
+        print("{:d}".format(number))
