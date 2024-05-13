b5b0fea9d5123ba0e9c152f299723ffdaff9eb5d
Guillom8769 <8769@holbertonstudents.com>
2024-05-13 16:13:17 +0200

    modify del function

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    """
-    Supprime un élément à une position spécifique dans une liste.
-
-    Args:
-        my_list (list): La liste dont un élément sera supprimé.
-        idx (int): L'indice de l'élément à supprimer.
-
-    Returns:
-        list: La liste modifiée après la suppression de l'élément.
-    """
     # Vérifier si l'indice est valide
-    if 0 <= idx < len(my_list):
-        # Supprimer l'élément en utilisant le découpage de liste
-        my_list = my_list[:idx] + my_list[idx+1:]
+    if idx < 0 or idx >= len(my_list):
+        return my_list  # Rien ne change, retourne la même liste
+
+    # Supprimer l'élément à l'indice spécifié
+    del my_list[idx]
 
     return my_list
