1c0b5c0d431c0378925064117a7320b02a92240d
Guillom8769 <8769@holbertonstudents.com>
2024-05-17 12:27:45 +0200

    class Square that defines a square by: (based on 4-square.py)

---
+++ python-classes/5-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+Module 5-square
+Ce module définit une classe Square avec un attribut privé pour représenter un carré.
+"""
+
+class Square:
+    """
+    Classe Square qui définit un carré avec un attribut privé size.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialise un nouveau carré avec une taille spécifiée.
+        :param size: Taille du carré (doit être un entier >= 0)
+        :raises TypeError: si size n'est pas un entier
+        :raises ValueError: si size est < 0
+        """
+        self.size = size  # Utilise le setter pour valider et assigner
+
+    @property
+    def size(self):
+        """
+        Getter pour l'attribut privé size.
+        :return: La taille du carré
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter pour l'attribut privé size avec vérification de type et de valeur.
+        :param value: La nouvelle taille du carré
+        :raises TypeError: si value n'est pas un entier
+        :raises ValueError: si value est < 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calcule et retourne l'aire du carré.
+        :return: Aire du carré
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        Affiche le carré avec le caractère # dans stdout.
+        Si la taille est 0, imprime une ligne vide.
+        """
+        if self.__size == 0:
+            print("")
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
