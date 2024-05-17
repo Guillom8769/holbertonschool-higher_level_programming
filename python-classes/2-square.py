3a0b25fbf9333e8c64dc57f80fb1b6ab78efd14b
Guillom8769 <8769@holbertonstudents.com>
2024-05-17 12:08:47 +0200

    class Square that defines a square by: (based on 1-square.py)

---
+++ python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module 2-square
+Ce module définit une classe Square avec un attribut privé pour représenter un carré.
+"""
+
+class Square:
+    """
+    Classe Square qui définit un carré avec un attribut privé size.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialise un nouveau carré avec une taille spécifiée.
+        :param size: Taille du carré (doit être un entier >= 0)
+        :raises TypeError: si size n'est pas un entier
+        :raises ValueError: si size est < 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size  # Attribut d'instance privé
