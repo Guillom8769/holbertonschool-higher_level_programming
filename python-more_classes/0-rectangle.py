7a6b7cf37d0770d6bfff845bfe665a794cc34894
Guillom8769 <8769@holbertonstudents.com>
2024-05-21 16:18:12 +0200

    an empty class Rectangle that defines a rectangle:

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module 0-rectangle
+Ce module fournit une classe `Rectangle` qui définit un rectangle.
+"""
+
+
+class Rectangle:
+    """Classe qui définit un rectangle."""
+    pass
