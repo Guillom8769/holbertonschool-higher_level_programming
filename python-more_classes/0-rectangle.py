6a11074aa7b040aad586417975ed2369dbe35452
Guillom8769 <8769@holbertonstudents.com>
2024-05-21 21:17:06 +0200

    modif pycode

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Module 0-rectangle
-Ce module fournit une classe `Rectangle` qui définit un rectangle.
-"""
+"""Ce module fournit une classe `Rectangle` qui définit un rectangle."""
 
 
 class Rectangle:
