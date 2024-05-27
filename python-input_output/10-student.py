3a89fa97df7fba1e69c2c1194fc0a119d2e94a7a
Guillom8769 <8769@holbertonstudents.com>
2024-05-27 12:57:31 +0200

    class Student that defines a student by: (based on 9-student.py)
        Public instance attributes:
            first_name
            last_name
            age
        Instantiation with first_name, last_name and age: def __init__(self, first_name, last_name, age):
        Public method def to_json(self, attrs=None): that retrieves a dictionary representation of a Student instance (same as 8-class_to_json.py):
            If attrs is a list of strings, only attribute names contained in this list must be retrieved.
            Otherwise, all attributes must be retrieved

---
+++ python-input_output/10-student.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+Module that defines a class Student.
+"""
+
+
+class Student:
+    """
+    A class that defines a student by:
+    - Public instance attributes: first_name, last_name, age
+    - Instantiation with first_name, last_name and age
+    - Public method to_json that retrieves a dictionary representation
+      of a Student instance
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a new Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Args:
+            attrs (list): A list of attribute names to retrieve.
+
+        Returns:
+            dict: The dictionary representation of the Student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: self.__dict__[attr]
+                    for attr in attrs if attr in self.__dict__}
