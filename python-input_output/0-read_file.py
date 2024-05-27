c5d59a35b0d686cedba5f2c651e555568cb58ab6
Guillom8769 <8769@holbertonstudents.com>
2024-05-27 10:29:57 +0200

    unction that reads a text file (UTF8) and prints it to stdout:

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 0-read_file
+Defines a function that reads a text file and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename (str): The name of the file to read.
+          Default is an empty string.
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end='')
